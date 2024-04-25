ae03cca5332722d4d6a6f5510f6563baa047152b
David Vlastuin <davidvlas215@gmail.com>
2024-04-25 11:08:08 +0200

    v1

---
+++ appointment.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <main>
+        <?php require_once('includes/nav.php'); ?>
+    </main>    
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="js/app.js"></>
+</body>
+</html>
