1998fd7dc31827bbd0c91d02c386b881ed460ad5
29101994-Najibee <96542800+29101994-Najibee@users.noreply.github.com>
2024-05-24 14:22:18 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,3 @@
-
-@@ -1,128 +1,59 @@
 <?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -20,6 +18,7 @@ require_once 'includes/login_view.inc.php';
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=MuseoModerno:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
@@ -27,132 +26,110 @@ require_once 'includes/login_view.inc.php';
 </head>
 
 <body>
-  
+
     <main>
 
-    <?php require_once ('includes/nav.php'); ?>
+        <?php require_once('includes/nav.php'); ?>
 
-        <section class="container">
-            <div class="row g-5 pt-5">
-                <div class="col-md-8 offset-md-2 col-lg-8">
-                    <div class="container align-items-center" style="margin-top: 8rem;">
-                    <?php check_signup_errors(); ?>
-                        <h2 class="tittle">Registeer Form</h2>
-                        <form action="includes/signup.inc.php" method="post">
-                            <div class="form-row">
-                                <div class="col">
-                                    <label>Voornaam</label>
-                                    <input type="text" class="form-control" name="Firstname" id="Firstname" placeholder="Voornaam" value="<?php echo isset($_SESSION['signup_data']['Firstname']) ? htmlspecialchars($_SESSION['signup_data']['Firstname']) : ''; ?>">
-                                </div>
-                                <div class="col">
-                                    <label>Achternaam</label>
-                                    <input type="text" class="form-control" name="Lastname" id="Lastname" placeholder="Achternaam" value="<?php echo isset($_SESSION['signup_data']['Lastname']) ? htmlspecialchars($_SESSION['signup_data']['Lastname']) : ''; ?>">
-                                </div>
-                            </div>
-                            <div class="form-row">
-                                <div class="col">
-                                    <label>Email</label>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="email"><i class="fa-regular fa-envelope"></i></span>
-                                        </div>
-                                        <input type="email" class="form-control" name="Mail" id="Mail" placeholder="Email" value="<?php echo isset($_SESSION['signup_data']['Mail']) ? htmlspecialchars($_SESSION['signup_data']['Mail']) : ''; ?>">
-                                    </div>
-                                </div>
-                                <div class="col">
-                                    <label>Telefoon</label>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="telefoon"><i class="fa-solid fa-phone"></i></span>
-                                        </div>
-                                        <input type="text" class="form-control" name="PhoneNumber" id="PhoneNumber" placeholder="telefoon" value="<?php echo isset($_SESSION['signup_data']['PhoneNumber']) ? htmlspecialchars($_SESSION['signup_data']['PhoneNumber']) : ''; ?>">
-                <div class="row g-5 pt-5">
-                    <div class="col-md-8 offset-md-2 col-lg-8">
-                        <h1 class="mb-3">Registeren</h1>
-                        <form class="needs-validation" method="post">
-                            <div class="row g-3">
-                                <div class="col-12">
-                                    <label for="firstName" class="form-label">Je naam*</label>
-                                    <input type="text" class="form-control" id="firstName" placeholder="" value=""
-                                        required>
-                                    <div class="invalid-feedback">
-                                        Valid first name is required.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-row">
-                                <div class="col">
-                                    <label>Straat</label>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="straat"><i class="fa-solid fa-location-dot"></i></span>
-                                        </div>
-                                        <input type="text" class="form-control" name="Streetname" id="StreetName" placeholder="Straat Naam" value="<?php echo isset($_SESSION['signup_data']['Streetname']) ? htmlspecialchars($_SESSION['signup_data']['Streetname']) : ''; ?>">
-                                        <input type="text" class="form-control" name="HouseNumber" id="Housenumber" placeholder="Huis nummer" value="<?php echo isset($_SESSION['signup_data']['HouseNumber']) ? htmlspecialchars($_SESSION['signup_data']['HouseNumber']) : ''; ?>">
-                                    </div>
-                                </div>
-                                <div class="col-md-4 mb-3">
-                                    <label>Postcode</label>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="Zipcode"><i class="fa-solid fa-door-closed"></i></span>
-                                        </div>
-                                        <input type="text" class="form-control" name="Zipcode" id="Zipcode" placeholder="Postcode" value="<?php echo isset($_SESSION['signup_data']['Zipcode']) ? htmlspecialchars($_SESSION['signup_data']['Zipcode']) : ''; ?>">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-row">
-                                <div class="col">
-                                    <label>Password</label>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="password"><i class="fa-solid fa-lock"></i></span>
-                                        </div>
-                                        <input type="password" class="form-control" name="Password" id="Password" placeholder="Password">
-                                    </div>
-                                </div>
-                                <div class="col">
-                                    <label>Bevestig Password</label>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="cpassword"><i class="fa-solid fa-key"></i></span>
-                                        </div>
-                                        <input type="password" class="form-control" name="cpassword" id="cpassword" placeholder="Bevestig Password">
-
-                                <div class="col-12">
-                                    <label for="email" class="form-label">Email*</label>
-                                    <input type="email" class="form-control" id="email" placeholder="you@example.com" required>
-                                    <div class="invalid-feedback">
-                                        Please enter a valid email address for shipping updates.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group row" style="margin-top:15px">
-                                <div class="col-sm-10">
-                                    <button class="btn btn-primary" type="submit">Verzenden</button>
-                                </div>
-                            </div>
-
-                            <hr class="my-4">
-
-                            <button class="w-100 btn btn-primary btn-lg" type="submit">Verzenden</button>
-                        </form>
+        <div class="container mt-3">
+            <form action="includes/signup.inc.php" method="post">
+                <?php check_signup_errors(); ?>
+                <div class="row jumbotron box8">
+                    <div class="col-sm-12 mx-t3 mb-4">
+                        <h2 class="text-center text-info">Register</h2>
                     </div>
+                    <div class="col-sm-6 form-group">
+                        <label for="firstname">First Name</label>
+                        <input type="text" class="form-control" name="Firstname" id="Firstname" placeholder="Voornaam">
+                    </div>
+                    <div class="col-sm-6 form-group">
+                        <label for="lastname">Last name</label>
+                        <input type="text" class="form-control" name="Lastname" id="Lastname" placeholder="Achternaam">
+                    </div>
+                    <div class="col-sm-6 form-group">
+                        <label for="email">Email</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="email"><i class="fa-regular fa-envelope"></i></span>
+                            </div>
+                            <input type="email" class="form-control" name="Mail" id="Mail" placeholder="Email">
+                        </div>
+                    </div>
+                    <div class="col-sm-6 form-group">
+                        <label for="telefoon">Telefoon</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="telefoon"><i class="fa-solid fa-phone"></i></span>
+                            </div>
+                            <input type="text" class="form-control" name="PhoneNumber" id="PhoneNumber" placeholder="telefoon">
+
+                        </div>
+                    </div>
+                    <div class="col-sm-6 form-group">
+                        <label for="straat">Straat</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="straat"><i class="fa-solid fa-location-dot"></i></span>
+                            </div>
+                            <input type="text" class="form-control" name="Streetname" id="Streetname" placeholder="Straat Naam">
+                        </div>
+                    </div>
+                    <div class="col-sm-4 form-group">
+                        <label for="huisnummer">Huisnumer</label>
+                        <div class="input-group">
+                        <div class="input-group-prepend">
+                                <span class="input-group-text" id="straat"><i class="fa-solid fa-door-closed"></i></span>
+                            </div>
+                            <input type="text" class="form-control" name="HouseNumber" id="HouseNumber" placeholder="Huisnummer">
+                        </div>
+                        </div>
+                    <div class="col-sm-2 form-group">
+                        <label for="zip">Post-Code</label>
+                        <input type="text" class="form-control" name="Zipcode" id="Zipcode" placeholder="Postcode">
+                    </div>
+
+                    <div class="col-sm-6 form-group">
+                        <label for="pass">Password</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="email"><i class="fa-solid fa-lock"></i></span>
+                            </div>
+                            <input type="password" class="form-control" name="Password" id="Password" placeholder="Password">
+                        </div>
+                        
+                    </div>
+                    <div class="col-sm-6 form-group">
+                        <label for="pass2">Confirm Password</label> 
+                         <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="email"><i class="fa-solid fa-lock"></i></span>
+                            </div>
+                            <input type="password" class="form-control" name="cpassword" id="cpassword" placeholder="Confirm Password">
+                    </div>
+                        </div>
+                        
+                    <div class="col-sm-12">
+                        <input type="checkbox" class="form-check d-inline" id="chb"><label for="chb" class="form-check-label">&nbsp;I accept all terms and conditions.
+                    </div>
+                    <div class="col-sm-12">
+                        <p>Heb je al account ! <a href="login.php">Inloggen</a></p>
+                       
+                    </div>
+
+                    <div class="col-sm-12 form-group mb-0">
+                        <button class="btn btn-primary float-right">Submit</button>
+                    </div>
+
                 </div>
-            </div>
-        </section>
-
-     
-        <?php require_once ('includes/footer.php'); ?>
-
+            </form>
+        </div>
+        <?php require_once('includes/footer.php'); ?>
+        <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="js/app.js"></script>
     </main>
-
-    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="js/app.js"></script>
 </body>
+
 </html>
 <?php
 unset($_SESSION['signup_data']);
 ?>
-</body >
-</html >
